083b08c545276ac3af6701b9a1e41648c3c20ea6
Jimmy Nguyen <jimmyn123@gmail.com>
2018-09-12 09:35:50 -0700

    made the devil listen

--- richard-jimmy/server.js
+++ richard-jimmy/server.js
@@ -1,12 +1,13 @@
 'use strict';
 
 const express = require('express');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 6667;
 
 // REVIEW: POST route needs to parse the body passed in with the request.
 // POST middleware
 app.use(express.urlencoded({ extended: true }));
 
+app.listen(PORT, () => console.log(`The devil is listening on port ${PORT}`));
 
 // this sets up express to serve any file in the public folder allowing requests to access every file in it.
 app.use(express.static('public'));
