2f297b29e3ef588c8d5e1ecc55e614821f7248da
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-09-12 09:45:20 -0700

    Add hello world response to root path

--- rebecca-brian-dominic/server.js
+++ rebecca-brian-dominic/server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+// const cors = require('cors');
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -9,8 +10,16 @@ const app = express();
 // POST middleware
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/', (request, response) => {
+  response.send('hello world!');
+})
+
 app.post('/articles', (request, response) => {
   // REVIEW: This route will receive a new article from the form page, new.html, and log that form data to the console. We will wire this up soon to actually write a record to our persistence layer!
   console.log(request.body);
   response.status(201).json(request.body);
+})
+
+app.listen(3000, () => {
+  console.log('things are happening!!');
 })
