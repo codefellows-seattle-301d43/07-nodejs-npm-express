e9c0723d5d4808124ef952d542cc6c7d3cec188d
alstof <alstof@gmail.com>
2018-09-12 09:09:19 -0700

    Add express app

--- kris-alex/server.js
+++ kris-alex/server.js
@@ -1,14 +1,21 @@
 'use strict';
 
 const express = require('express');
+const debug = require('debug');
 const PORT = process.env.PORT || 3000;
 
 // REVIEW: POST route needs to parse the body passed in with the request.
 // POST middleware
+let app = express();
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
 app.post('/articles', (request, response) => {
   // REVIEW: This route will receive a new article from the form page, new.html, and log that form data to the console. We will wire this up soon to actually write a record to our persistence layer!
   console.log(request.body);
   response.status(201).json(request.body);
-})
+});
+
+app.listen(PORT, () => {
+  debug(`Listening on ${PORT}`);
+});
