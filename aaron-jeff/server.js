dc21ac43d1aa2a1fb7db57d514957462480a1225
Jeff <jhwe92@gmail.com>
2018-09-12 10:24:37 -0700

    Set up new path to serve new.html, set up 404 catcher for redirect

--- aaron-jeff/server.js
+++ aaron-jeff/server.js
@@ -10,10 +10,25 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
+// app.get('/red', (request, response) => {
+//   response.redirect('/');
+// });
+
+app.get('/new', (request, response) => {
+  console.log(__dirname);
+  response.sendFile('new.html', {root: `${__dirname}/public`});
+});
+
+
 app.post('/articles', (request, response) => {
   // REVIEW: This route will receive a new article from the form page, new.html, and log that form data to the console. We will wire this up soon to actually write a record to our persistence layer!
   console.log(request.body);
   response.status(201).json(request.body);
+});
+
+app.get('*', (request, response) => {
+  console.log('404');
+  response.redirect('/');
 });
 
 app.listen(PORT , () => {
