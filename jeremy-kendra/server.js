874faff98e692211eb9c04d26aa76f0c6b9a9d1b
ellisken <ellisken@oregonstate.edu>
2018-09-12 11:17:55 -0700

    added comment about public folder

--- jeremy-kendra/server.js
+++ jeremy-kendra/server.js
@@ -7,8 +7,12 @@ const app = express();
 
 // REVIEW: POST route needs to parse the body passed in with the request.
 // POST middleware
+
 app.use(express.urlencoded({ extended: true }));
+
+// Our files are in a "public" directory so that we can use express to serve all of the files in that directory statically. Each file can then be accessed by a separate URI.
 app.use(express.static('public'));
+
 
 app.post('/articles', (request, response) => {
   // REVIEW: This route will receive a new article from the form page, new.html, and log that form data to the console. We will wire this up soon to actually write a record to our persistence layer!
@@ -17,7 +21,7 @@ app.post('/articles', (request, response) => {
 });
 
 // Respond with 404 error if non-existent page requested
-app.use(function(req,res){
+app.use((req,res) => {
   res.status(404);
   res.send('Invalid request: page does not exist');
 });
