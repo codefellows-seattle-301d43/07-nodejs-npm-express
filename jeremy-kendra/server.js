8d88284f61c3e5831de7dd7087976f4b587d449b
Jeremy Beck <jeremycbeck@outlook.com>
2018-09-12 09:32:45 -0700

    server working

--- jeremy-kendra/server.js
+++ jeremy-kendra/server.js
@@ -2,13 +2,20 @@
 
 const express = require('express');
 const PORT = process.env.PORT || 3000;
+// const cors = require('cors');
+const app = express();
 
 // REVIEW: POST route needs to parse the body passed in with the request.
 // POST middleware
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 app.post('/articles', (request, response) => {
   // REVIEW: This route will receive a new article from the form page, new.html, and log that form data to the console. We will wire this up soon to actually write a record to our persistence layer!
   console.log(request.body);
   response.status(201).json(request.body);
-})
+});
+
+app.listen(PORT, () => {
+  console.log(`server is running and listening on ${PORT}`);
+});
