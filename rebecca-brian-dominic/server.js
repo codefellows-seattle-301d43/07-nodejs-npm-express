443ce7be3e4a85d40e725f31236b809df40e88e1
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-09-12 11:14:39 -0700

    Add 404 error handler

--- rebecca-brian-dominic/server.js
+++ rebecca-brian-dominic/server.js
@@ -15,11 +15,10 @@ app.get('/', (request, response) => {
   response.send(request);
 });
 
-app.get('/articles/new', (req, res) => {
+app.get('/new', (req, res) => {
   // res.redirect('http://localhost:3000/new.html');
-  console.log(__dirname);
-  res.sendFile('public/data/hackerIpsum.json', {root: __dirname});
-})
+  res.sendFile('/new.html', { root: './public' });
+});
 
 // DONE: Our files are in a public directory now so that we can load the static files in our browser window.
 app.use(express.static('public'));
@@ -28,8 +27,17 @@ app.post('/articles', (request, response) => {
   // REVIEW: This route will receive a new article from the form page, new.html, and log that form data to the console. We will wire this up soon to actually write a record to our persistence layer!
   console.log(request.body);
   response.status(201).json(request.body);
-})
+});
+
+// STRETCH GOAL: HANDLE 404
+app.use((req, res) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  // res.status(404).send(err); // sends JSON { "status": 404 }
+  res.status(404);
+  res.sendFile('public/images/404cat.jpeg', { root: __dirname });
+});
 
 app.listen(3000, () => {
   console.log('things are on port 3000!!');
-})
+});
