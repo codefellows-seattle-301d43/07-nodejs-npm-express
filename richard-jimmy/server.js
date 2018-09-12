ec08481b4f493f3a990b347317e9dbb3eebc082a
richjimdev <richjimdev@gmail.com>
2018-09-12 11:11:40 -0700

    fixed new.html URI routing. Updated README

--- richard-jimmy/server.js
+++ richard-jimmy/server.js
@@ -11,9 +11,10 @@ app.use(express.urlencoded({ extended: true }));
 // this sets up express to serve any file in the public folder allowing requests to access every file in it.
 app.use(express.static('public'));
 
-app.get('/index.html', (request, response) => response.sendFile('index.html', {root: __dirname}));
-
-app.get('/new.html', (request, response) => response.sendFile('new.html', {root: __dirname}));
+app.get('/new', (request, response) => {
+  console.log(__dirname);
+  response.sendFile('/public/new.html', {root: __dirname});
+});
 
 app.post('/articles', (request, response) => {
   // REVIEW: This route will receive a new article from the form page, new.html, and log that form data to the console. We will wire this up soon to actually write a record to our persistence layer!
